94f95be0c623a4960736238d42ea782d11e7179c
juliorkm <julioramakrsnamandoju@gmail.com>
2017-05-20 13:10:21 -0300

    Sprites das Notas e da Linha de Certo
    - Sprites recoloríveis das notas;
    - Sprite recolorível da Linha de Certo;
    - Alinhamento dos mesmos na HUD.

--- Assets/Scripts/Nota.cs
+++ Assets/Scripts/Nota.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Nota : MonoBehaviour {
-    public enum Cor {
+public class Nota : MonoBehaviour
+{
+    public enum Cor
+    {
         BLACK, WHITE, YELLOW
     }
 
@@ -14,26 +16,38 @@ public class Nota : MonoBehaviour {
     private RectTransform rect;
     private Image img;
 
-    void Start () {
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
         rect = GetComponent<RectTransform>();
-        rb.velocity = new Vector2(speed, 0);
-        img = GetComponent<Image>();
+        rb.velocity = new Vector2(speed * Screen.width / 10, 0);
+
+        //Pegar a parte que deve ser colorida dentre os Images dos filhos
+        Image[] children = GetComponentsInChildren<Image>();
+        for (int i = 0; i < children.Length; i++)
+            if (children[i].gameObject.name.Equals("Color"))
+            {
+                img = children[i];
+                break;
+            }
     }
 
-    void Update() {
+    void Update()
+    {
         deleteOnExit();
         changeColor();
     }
 
-    void changeColor() {
-        if (cor == Cor.BLACK) img.color = Color.black;
+    void changeColor()
+    {
+        if (cor == Cor.BLACK) img.color = new Color(0.196f, 0.196f, 0.196f, 1f);
         if (cor == Cor.WHITE) img.color = Color.white;
-        if (cor == Cor.YELLOW) img.color = Color.yellow;
+        if (cor == Cor.YELLOW) img.color = new Color(0.906f, 0.788f, 0.306f, 1f);
 
     }
 
-    void deleteOnExit() {
+    void deleteOnExit()
+    {
         // O *1.5f eh gambiarra;
         // preciso saber quando o sprite inteiro está depois da tela, mas so sei a posicao do meio dele
         if (gameObject != null && rect.position.x > Screen.width * 1.5f) Destroy(gameObject);
